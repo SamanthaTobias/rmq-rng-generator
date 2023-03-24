5334ad5b97e24fcc56acebfc6a8ba6d792a28996
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-03-24 21:42:54 +0100

    move javascript to separate file in static/js/index.js

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('form').submit(function(e) {
+        e.preventDefault();
+        var action = $(this).attr('action');
+        var method = $(this).attr('method');
+        var data = $(this).serialize();
+        $.ajax({
+            url: action,
+            type: method,
+            data: data
+        });
+        return false;
+    });
+});
